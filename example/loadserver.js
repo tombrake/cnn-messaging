5a59baabbf3d839277c4ddf2436ddc4bdb728891
Ian Patton <ian.patton@turner.com>
2017-05-25 14:44:24 -0400

    Improve load testing server in example/

--- example/loadserver.js
+++ example/loadserver.js
@@ -1,16 +1,36 @@
 'use strict';
 
+const http = require('http');
+const express = require('express');
+const app = express();
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.get('/health', function (req, res) {
+    res.send({message: 'noop'});
+});
+
+const server = http.createServer(app);
+
 const Message = require('../index').Message;
 const amqpMessenger = require('../index').AmqpMessenger({
     amqp: {
         connectionString: process.env.AMQP_CONN_STRING || 'amqp://localhost:5672',
         exchangeName: process.env.AMQP_EXCHANGE || 'MOCHA_TEST'
     },
-    port: process.env.PORT || 13000
+    http: server
 });
 
-console.log('Starting on port', process.env.PORT || 13000);
-amqpMessenger.start();
+amqpMessenger.start()
+    .then(() => {
+        server.listen(process.env.PORT || 13000, function listening() {
+            console.log('Listening on %d', server.address().port);
+        });
+    });
 
 setInterval(() => {
     amqpMessenger.publish('test.message.new', new Message({
