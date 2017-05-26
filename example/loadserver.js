7314640e1b02a63bdc167f16f66711a4d61bbdbf
Ian Patton <ian.patton@turner.com>
2017-05-26 10:11:36 -0400

    load test server fix

--- example/loadserver.js
+++ example/loadserver.js
@@ -18,7 +18,8 @@ app.get('/health', function (req, res) {
 const server = http.createServer(app);
 
 const Message = require('../index').Message;
-const amqpMessenger = require('../index').AmqpMessenger({
+const Messenger = require('../index').AmqpMessenger;
+const messenger = new Messenger({
     amqp: {
         connectionString: process.env.AMQP_CONN_STRING || 'amqp://localhost:5672',
         exchangeName: process.env.AMQP_EXCHANGE || 'MOCHA_TEST'
@@ -33,7 +34,7 @@ metrics.init({
     flushEvery: 1000 * 2
 });
 
-amqpMessenger.start()
+messenger.start()
     .then(() => {
         server.listen(process.env.PORT || 13000, function listening() {
             console.log('Listening on %d', server.address().port);
@@ -41,11 +42,11 @@ amqpMessenger.start()
     });
 
 setInterval(() => {
-    metrics.histogram('wsconns', amqpMessenger.websocketRelay.connections);
+    metrics.histogram('wsconns', messenger.websocketRelay.connections);
 });
 
 setInterval(() => {
-    amqpMessenger.publish('test.message.new', new Message({
+    messenger.publish('test.message.new', new Message({
         event: {
             some: {
                 thing: Math.random()
@@ -55,9 +56,9 @@ setInterval(() => {
 }, (process.env.INTERVAL && parseInt(process.env.INTERVAL)) || 30000);
 
 setInterval(() => {
-    const subscribedTopics = Object.keys(amqpMessenger.websocketRelay.subscriptions);
+    const subscribedTopics = Object.keys(messenger.websocketRelay.subscriptions);
     console.log('Subscribed to', subscribedTopics.length, 'topics');
     subscribedTopics.forEach((topic) => {
-        console.log('- topic:', topic, 'subscribers:', Object.keys(amqpMessenger.websocketRelay.subscriptions[topic]).length);
+        console.log('- topic:', topic, 'subscribers:', Object.keys(messenger.websocketRelay.subscriptions[topic]).length);
     });
 }, 60000);
